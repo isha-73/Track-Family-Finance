71c26770300d4f6474e1b78de367f8f26c30c66a
Isha Bule <ishabule753@gmail.com>
2024-06-05 18:01:43 +0530

    connection added

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
-// Import required modules
 const express = require('express');
+const connectDB = require('./dbserver')
+const User = require ('./models/users')
+connectDB()
 
 // Create an Express application
 const app = express();
 
-// Define routes and middleware here
 
-// Start the server
+
+
+// To Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
